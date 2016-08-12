691ee30a38e19f9d2c2bd79ecc3c9dbb950d5514
Stéphane <diyfr@users.noreply.github.com>
2016-08-12 15:00:15 +0200

    Update SQLiteDialect.java

--- src/main/java/org/hibernate/dialect/SQLiteDialect.java
+++ src/main/java/org/hibernate/dialect/SQLiteDialect.java
@@ -5,6 +5,7 @@
  *    May you do good and not evil.
  *    May you find forgiveness for yourself and forgive others.
  *    May you share freely, never taking more than you give.
+ * From https://github.com/gwenn/sqlite-dialect
  *
  */
 package org.hibernate.dialect;
